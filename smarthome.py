34d8f8b9c7389a89f06c9499291fb07ef8abe07c
Jags23 <89766280+Jags23@users.noreply.github.com>
2021-09-13 00:27:46 +0530

    Update smarthome.py

--- smarthome.py
+++ smarthome.py
@@ -2,7 +2,7 @@ pip install adafruit-io
 pip install python-telegram-bot==13.0 
 from telegram.ext import Updater, MessageHandler, Filters
 from Adafruit_IO import Client
-client_name='JagadeepT'
+client_name='JagadeepT' 
 client_api='aio_gDkz94xRP7XPR7CwESQGuMIDkyYv'
 
 aio=Client(client_name,client_api)
